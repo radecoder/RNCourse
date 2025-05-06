3b3c6c0a454c6e765c2e07cd8cea23973010af54
radecoder <mohdsahildw@gmail.com>
2025-05-06 17:31:08 +0530

    created goal app

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My First React Native app</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputBox}>
+        <TextInput style={styles.textInput} placeholder="Enter Goals here" />
+        <Button title="Add goal" />
+      </View>
+      <View>
+        <Text>List Of Goals</Text>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+    backgroundColor: "#cccccc",
+    height: '100%'
   },
+  inputBox: {
+
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginRight: 4,
+
+
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'green',
+    width: '80%',
+    marginRight: 8,
+  }
 });
